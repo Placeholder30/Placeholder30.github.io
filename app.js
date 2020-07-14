543e5b553672f53f09a7ee5ff8c1d800baa00f58
Placeholder30 <malikoabdulaziz@gmail.com>
2020-07-14 14:15:37 +0100

    did the darkmode thing and it looks kinda neat

--- app.js
+++ app.js
@@ -1,15 +1,18 @@
 const toggle = document.querySelector("input");
 const topGrid = document.querySelectorAll(".grid");
 const bottomGrid = document.querySelectorAll(".main-grid");
+const dashboardHeader = document.querySelector(".dashboard-header");
+const overview = document.querySelector(".overview");
 
 toggle.addEventListener("click", () => {
   document.body.classList.toggle("dark");
   for (let i = 0; i < topGrid.length; i++) {
     topGrid[i].classList.toggle("dark");
   }
-
-  bottomGrid.classList.toggle("dark");
   for (let i = 0; i < bottomGrid.length; i++) {
     bottomGrid[i].classList.toggle("dark");
   }
+
+  dashboardHeader.classList.toggle("dark");
+  overview.classList.toggle("dark");
 });
