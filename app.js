92f84af3e452b39e3ea29da6c3280bfddd878844
Placeholder30 <malikoabdulaziz@gmail.com>
2020-07-14 17:05:05 +0100

    did a toggle button for dark mode

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
-const toggle = document.querySelector("input");
+const toggle = document.querySelector(".square");
+const circle = document.querySelector(".circle");
 const topGrid = document.querySelectorAll(".grid");
 const bottomGrid = document.querySelectorAll(".main-grid");
 const dashboardHeader = document.querySelector(".dashboard-header");
 const overview = document.querySelector(".overview");
 
 toggle.addEventListener("click", () => {
+  circle.classList.toggle("active");
   document.body.classList.toggle("dark");
   for (let i = 0; i < topGrid.length; i++) {
     topGrid[i].classList.toggle("dark");
